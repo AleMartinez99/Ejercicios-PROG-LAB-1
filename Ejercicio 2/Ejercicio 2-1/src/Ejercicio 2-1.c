fcb77016f6003934590d12d0902b63919e2f4151
Alejandro Martinez <77170958+AleMartinez99@users.noreply.github.com>
2022-03-31 21:46:43 -0300

    Update Ejercicio 2-1.c

--- Ejercicio 2/Ejercicio 2-1/src/Ejercicio 2-1.c
+++ Ejercicio 2/Ejercicio 2-1/src/Ejercicio 2-1.c
@@ -1,11 +1,7 @@
 /*
- ============================================================================
- Name        : Ejercicio.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
+Ejercicio 2-1: Ingresar 5 números enteros calcular y mostrar el promedio de los números. Probar la aplicación con distintos valores.
+Ejemplo 1:  3 - 5 - 7 - 9 - 1
+Ejemplo 2:  2 - 1 - 8 -1 - 2
  */
 
 #include <stdio.h>
